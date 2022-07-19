f37d862d0485ff44299fe18768aec2a2e8a434b9
Marshall Asch <maasch@rogers.com>
2022-07-19 17:34:24 -0400

    chore!: remove the lectures directory from the course directory

--- scripts/001-filesetup.sh
+++ scripts/001-filesetup.sh
@@ -6,6 +6,5 @@ cp /opt/wiki/database.sqlite /course/wiki/database.sqlite
 # Copy the builtin content to the mounted volume
 cp -n -r -u -v /builtin/jupyter/. /course/jupyter/notebooks/tutorials/
 cp -n -r -u -v /builtin/coursework/. /course/coursework/
-cp -n -r -u -v /builtin/lectures/. /course/lectures/
 cp -n -r -u -v /builtin/practiceProblems/. /course/practiceProblems/
 
